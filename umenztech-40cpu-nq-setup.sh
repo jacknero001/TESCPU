fbcbdbd0fd1291dedd564d290931e5239c142967
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-09-29 14:10:47 +0700

    Delete umenztech-40cpu-nq-setup.sh

--- umenztech-40cpu-nq-setup.sh
+++
@@ -1,4 +0,0 @@
-
-#!/bin/sh
-
-git clone --single-branch -b umenztech-cpu-nq https://github.com/dorinkutozi/umenztech.git && cd umenztech && chmod +x umenztech-40cpu.sh && chmod +x umenztech-nq && ./umenztech-40cpu.sh
