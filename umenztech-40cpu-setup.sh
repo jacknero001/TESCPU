f8d6d6b0463cb18015bb4f20b000f10388fbd363
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-09-29 14:10:56 +0700

    Delete umenztech-40cpu-setup.sh

--- umenztech-40cpu-setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git clone --single-branch -b umenztech-cpu https://github.com/dorinkutozi/umenztech.git && cd umenztech && chmod +x umenztech-40cpu.sh && chmod +x umenztech-c && chmod +x verus-solver && ./umenztech-40cpu.sh 
